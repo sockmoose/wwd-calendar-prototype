f167cba80f246cd3dbdb5542f622cb6a8f26ba13
Matt Hiebert <mindahr@hotmail.com>
2017-07-02 19:52:46 -0500

    Add functionality to mock data service

--- src/app/calendar/mock-data.service.ts
+++ src/app/calendar/mock-data.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 
 const DATA = {
-
+  data: [
+    {date: '3/7/2017', garbage: 'show', recycling: 'show', yardWaste: 'show'},
+    {date: '17/7/2017', garbage: 'show', recycling: 'show'},
+    {date: '31/7/2017', garbage: 'show', recycling: 'show', yardWaste: 'show'},
+  ]
 };
 
 @Injectable()
 export class MockDataService {
-
-  constructor() { }
+  getPickupData(month: number, year: number): any {
+    if (month === 7 && year === 2017) {
+      return DATA;
+    }
+  }
 }
