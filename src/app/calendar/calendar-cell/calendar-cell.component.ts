d650f0f8996c06f5fb2429ea3a5e583284eb4af7
Matt Hiebert <mindahr@hotmail.com>
2017-07-02 23:07:53 -0500

    Add OnInit to calendar cell, edit tslint

--- src/app/calendar/calendar-cell/calendar-cell.component.ts
+++ src/app/calendar/calendar-cell/calendar-cell.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calendar-cell',
   templateUrl: 'calendar-cell.component.html'
 })
-export class CalendarCellComponent {
+export class CalendarCellComponent implements OnInit {
+  @Input() date: string;
+  @Input() garbage: string;
+  @Input() recycling: string;
+  @Input() yardWaste: string;
+  dateObject: Date;
+  daysBeforeFirst: number[];
+  daysAfterLast: number[];
 
+  ngOnInit(): void {
+    const dateArray = this.date.split('/');
+    this.dateObject = new Date(parseInt(dateArray[2]), parseInt(dateArray[1]) + 1, parseInt(dateArray[0]));
+
+    switch (this.dateObject.getDate()) {
+      case 1:
+        for (let i = 0; i < this.dateObject.getDay(); i++) {
+          this.daysBeforeFirst.push(i);
+        }
+        break;
+      case 28:
+      case 29:
+      case 30:
+      case 31:
+        const tempDate = new Date(this.dateObject.getMilliseconds());
+        tempDate.setDate(this.dateObject.getDate() + 1);
+        if (tempDate.getMonth() !== this.dateObject.getMonth()) {
+          for (let i = 0; i < this.dateObject.getDay(); i++) {
+            this.daysAfterLast.push(i);
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { CalendarCellComponent } from './calendar-cell/calendar-cell.component';
+
+import { MockDataService } from './mock-data.service';
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html'
 })
 export class CalendarComponent {
+
+  constructor(private mockDataService: MockDataService) {}
+
 
 }
